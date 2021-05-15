0f78f877017933138577c44cda9a3abdd024d898
anilkaracay <anil.karacay06@hotmail.com>
2021-05-15 19:08:58 +0300

    Initial Commit

---
+++ InstagramMobileApp/InstagramMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramMobileAppApp.swift
+//  InstagramMobileApp
+//
+//  Created by Macbook Air on 15.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
